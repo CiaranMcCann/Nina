1fea45303a2010e6b167090c42f91a0a6208f1c0
R0nan07 <ronan.farrell@hotmail.com>
2013-02-04 18:55:20 +0100

    Parsing support for coins

--- src/Level.ts
+++ src/Level.ts
@@ -9,27 +9,25 @@ class Level
     {
         // Parers level
         var level = JSON.parse(levelData);
-        for (var i in level["platforms"]) {
+        var i;
+        for (i in level["platforms"]) {
             var x = level["platforms"][i].x;
             var y = level["platforms"][i].y;
             var w = level["platforms"][i].w / 2;
             var h = level["platforms"][i].h / 2;
             var p = new Platform(x + w, y + h, w, h);
         }
+        
+        this.coins = [];
+        for (i in level['waterCoins']) {
+            this.coins.push(new Coin(level['waterCoins'][i].x, level['waterCoins'][i].y, Coin.COIN_TYPE.water));
+        }
+        for (i in level['elecCoins']) {
+            this.coins.push(new Coin(level['elecCoins'][i].x, level['elecCoins'][i].y, Coin.COIN_TYPE.electity));
+        }
 
         this.walter = new Walter(level['walter'].x, level['walter'].y);
         this.alex = new Alex(level['alex'].x, level['alex'].y);
-
-        //Create a load of random coins
-        this.coins = [];
-        for(var k = 0; k < 10; k++)
-        {
-            this.coins.push(new Coin(
-                 Utilies.random(100, 3000),
-                Utilies.random(100, 3000),
-                Utilies.pickRandom([Coin.COIN_TYPE.electity, Coin.COIN_TYPE.water])
-            ));
-        }
     }
 
     draw(ctx)
